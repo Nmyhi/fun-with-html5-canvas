2726e3ea6e926fa395652cf8da810d4b8f07903f
Mihaly Lovrencsics <lomysa@gmail.com>
2024-07-27 22:15:02 +0000

    Add context, size attribute and isDrawing variable set to false

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false; // it will only draw if I push the mouse button down
