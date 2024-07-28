b7a88656abad801435397fd9d24be90fa94688b2
Mihaly Lovrencsics <lomysa@gmail.com>
2024-07-28 23:04:49 +0000

    Add variables to draw, add mouseout, mousedown and mouseup event listeners

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,8 +11,18 @@ let isDrawing = false; // it will only draw if I push the mouse button down
 let lastX = 0;
 let lastY = 0;
 
+
 function draw(e) {
+    if(!isDrawing) return; // stop the fn from running when they are not moused down
     console.log(e);
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
 }
 
+
 canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
